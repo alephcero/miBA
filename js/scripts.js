83a415793609035b5bb84d94c2d529dd1147038a
alephcero <4lephcero@gmail.com>
2017-03-10 17:44:08 -0500

    more variables incorporated

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var geojson;
-
+var currentMap = 'Empty'; 
 
 //Coropleth function
 function getColor(d) {
@@ -18,9 +18,9 @@ function getColor(d) {
 }
 
 //Style function that takes the Coropleth 
-function style(feature) {
+function style(feature,propiedad) {
     return {
-        fillColor: getColor(feature.properties.q),
+        fillColor: getColor(propiedad),//takes the property of the feature as parameter
         weight: .5,
         opacity: 1,
         color: 'white',
@@ -48,7 +48,20 @@ function highlightFeature(e) {
 }
 
 function resetHighlight(e) {
-    geojson.resetStyle(e.target);
+    var layer = e.target;
+
+    //if for the property that we wan to reset
+    if (currentMap == 'Education') {var propReset = layer.feature.properties.P19_q}
+    if (currentMap == 'Inmigration') {var propReset = layer.feature.properties.P05_q}
+    if (currentMap == 'Celular') {var propReset = layer.feature.properties.H2819C_q}
+    if (currentMap == 'Computer') {var propReset = layer.feature.properties.H2819B_q}
+    if (currentMap == 'Empty') {var propReset = layer.feature.properties.V02_q}
+    if (currentMap == 'Owner') {var propReset = layer.feature.properties.PROP_q}
+    if (currentMap == 'Rent') {var propReset = layer.feature.properties.INQ_q}
+    if (currentMap == 'Regular') {var propReset = layer.feature.properties.TENREG_q}
+
+
+    layer.setStyle(style(layer.feature,propReset));
     info.update();
 }
 
@@ -80,9 +93,11 @@ info.update = function (props) {
     this._div.innerHTML = '<h4>Census Block Information</h4>' +  (props ?
         '<b> Block ID: </b>' + props.REDCODE + '<br />' + 
         '<b> Comune: </b>' + props.Comune + '<br />' + 
-        '<b> % of Head of household with college education: </b>' + props.University  + '<br />' +
-        '<b> Quantile: </b>' + props.q  + '<br />'
+        '<b> % of Head of household with college education: </b>' + props.P19_p  + '<br />' +
+        '<b> Quantile: </b>' + props.P19_q  + '<br />'
         : 'Hover over a state');
+
+    //FALTA PONER IN IF ACA PAR DAR METRICAS DIFERENTES PARA CADA currentMap, SI ES empty NADA DE LO ABAJO
 };
 
 
@@ -114,3 +129,94 @@ geojson = L.geoJson(tablaDeDatos,{
 geojson.addTo(map);
 info.addTo(map);
 legend.addTo(map);
+
+
+//listener with property of the feature as a parameter
+
+$('#buttonEduc').on('click', function(){
+    currentMap = 'Education';
+
+    geojson.eachLayer(function (layer) {
+       layer.setStyle(style(layer.feature,layer.feature.properties.P19_q));
+       //console.log(layer.feature.properties.Comune)  
+    });
+});
+
+
+$('#buttonInmigration').on('click', function(){
+    //set the current visualization we are dealing with
+    currentMap = 'Inmigration'; 
+
+    geojson.eachLayer(function (layer) {
+       layer.setStyle(style(layer.feature,layer.feature.properties.P05_q));
+       
+       //console.log(layer.feature.properties.Comune)  
+    });
+});
+
+
+$('#buttonCel').on('click', function(){
+    //set the current visualization we are dealing with
+    currentMap = 'Celular'; 
+
+    geojson.eachLayer(function (layer) {
+       layer.setStyle(style(layer.feature,layer.feature.properties.H2819C_q));
+       
+       //console.log(layer.feature.properties.Comune)  
+    });
+});
+
+$('#buttonComp').on('click', function(){
+    //set the current visualization we are dealing with
+    currentMap = 'Computer'; 
+
+    geojson.eachLayer(function (layer) {
+       layer.setStyle(style(layer.feature,layer.feature.properties.H2819B_q));
+       
+       //console.log(layer.feature.properties.Comune)  
+    });
+});
+
+$('#buttonEmpty').on('click', function(){
+    //set the current visualization we are dealing with
+    currentMap = 'Empty'; 
+
+    geojson.eachLayer(function (layer) {
+       layer.setStyle(style(layer.feature,layer.feature.properties.V02_q));
+       
+       //console.log(layer.feature.properties.Comune)  
+    });
+});
+
+$('#buttonOwner').on('click', function(){
+    //set the current visualization we are dealing with
+    currentMap = 'Owner'; 
+
+    geojson.eachLayer(function (layer) {
+       layer.setStyle(style(layer.feature,layer.feature.properties.PROP_q));
+       
+       //console.log(layer.feature.properties.Comune)  
+    });
+});
+
+$('#buttonRent').on('click', function(){
+    //set the current visualization we are dealing with
+    currentMap = 'Rent'; 
+
+    geojson.eachLayer(function (layer) {
+       layer.setStyle(style(layer.feature,layer.feature.properties.INQ_q));
+       
+       //console.log(layer.feature.properties.Comune)  
+    });
+});
+
+$('#buttonRegular').on('click', function(){
+    //set the current visualization we are dealing with
+    currentMap = 'Regular'; 
+
+    geojson.eachLayer(function (layer) {
+       layer.setStyle(style(layer.feature,layer.feature.properties.TENREG_q));
+       
+       //console.log(layer.feature.properties.Comune)  
+    });
+});
